294503c27f1a58d8f478d6f8fbf217cdceaa7b16
jordan <jordan.washburn@gmail.com>
2022-06-18 18:16:52 -0600

    ffmpeg build changes

--- wasm/build-scripts/build-ffmpeg.sh
+++ wasm/build-scripts/build-ffmpeg.sh
@@ -30,7 +30,10 @@ FLAGS=(
   -s EXPORT_NAME="createFFmpegCore"             # assign export name for browser
   -s EXPORTED_FUNCTIONS="[_main]"  # export main and proxy_main funcs
   -s EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
-  -s INITIAL_MEMORY=2146435072                  # 64 KB * 1024 * 16 * 2047 = 2146435072 bytes ~= 2 GB
+  -s INITIAL_MEMORY=33554432                    # 32MB  
+  -s MAXIMUM_MEMORY=1073741824                  # 1GB 
+  -s ALLOW_MEMORY_GROWTH=1
+
   --pre-js wasm/src/pre.js
   --post-js wasm/src/post.js
   $OPTIM_FLAGS
